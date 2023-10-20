22f98cceccba7195999b756b118d242c853e7d42
KsHuyZ <92804746+KsHuyZ@users.noreply.github.com>
2023-10-20 23:36:45 +0700

    fix card number check

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -83,7 +83,7 @@ const userCtrl = {
       let cardNumber = null;
       while (!cardNumber) {
         const number = generateRandomNumber(16);
-        const userCard = await User.findOne({ number });
+        const userCard = await User.findOne({cardNumber:number });
 
         if (!userCard) {
           cardNumber = number;
@@ -122,123 +122,6 @@ const userCtrl = {
       return res.status(200).json({ success: true, user: newUser, token });
     } catch (error) {
       return res.status(400).json({ success: false, message: error.message });
-    }
-  },
-
-  removeUser: async (req, res) => {
-    const id = req.params.id;
-    try {
-      const user = await User.findByIdAndDelete(id);
-      return res.status(200).json({
-        success: true,
-        user,
-      });
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  showUserBelongToDepartment: async (req, res) => {
-    const q = req.params.departmentId;
-    try {
-      const data = await User.find({
-        idDepartment: q,
-      });
-
-      if (data) {
-        return res.status(200).json({
-          success: true,
-          data,
-        });
-      } else {
-        return res.status(400).json({
-          success: false,
-          msg: "fails",
-        });
-      }
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  showUserNotDepartment: async (req, res) => {
-    try {
-      const users = await User.find();
-      if (users) {
-        const data = users.filter((item) => !item.idDepartment);
-        return res.status(200).json(data);
-      }
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  addUserDepartment: async (req, res) => {
-    const idPerson = req.params.idPerson;
-    const { idDepartment } = req.body;
-    try {
-      const updatedPost = await User.findOneAndUpdate(
-        { _id: idPerson },
-        {
-          idDepartment,
-        }
-      );
-      if (updatedPost) {
-        res.status(200).json({ message: "Update successfully" });
-      } else {
-        console.log(updatedPost);
-      }
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  removeUserBelongToDepartment: async (req, res) => {
-    const id = req.params.deleteId;
-    try {
-      const updatedData = await User.findOneAndUpdate(
-        { _id: id },
-        {
-          idDepartment: null,
-        }
-      );
-      if (updatedData) {
-        res.status(200).json({ message: "Update successfully" });
-      } else {
-        console.log(updatedData);
-      }
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  searchEmailHasDepartment: async (req, res) => {
-    const departmentId = req.params.departmentId;
-    try {
-      const data = await User.find(
-        {
-          email: { $regex: req.params.q },
-          departmentId: departmentId,
-        },
-        "email name avatar role gender address position"
-      ).exec();
-
-      return res.status(200).json({
-        success: true,
-        data,
-      });
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  },
-  searchEmailHasNotDepartment: async (req, res) => {
-    try {
-      try {
-        const users = await User.find({ email: { $regex: req.params.q } });
-        if (users) {
-          const data = users.filter((item) => !item.idDepartment);
-          return res.status(200).json(data);
-        }
-      } catch (error) {
-        console.log("error: ", error);
-      }
-    } catch (error) {
-      console.log("error: ", error);
     }
   },
 };
